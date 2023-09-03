1e16e85e8a9387009de2df6fd5ddf836c1137d9d
thomas-girard <t.girard912@laposte.net>
2023-09-03 17:44:07 +0200

    improvement python file

--- optimisation_locale.py
+++ optimisation_locale.py
@@ -34,7 +34,14 @@ for filename in list_file:
     try:
         list_mdp, compteur_seconde = create_set(filename, limite_seconde)
         if len(set(list_mdp)) != 0:
-            ensemble_objets[filename] = {"ensemble":set(list_mdp), "poids":compteur_seconde}
+
+            if " " not in filename:
+                filename2 = f"brute-force {filename} characters"
+
+            else:
+                filename2 = filename
+
+            ensemble_objets[filename2] = {"ensemble":set(list_mdp), "poids":compteur_seconde}
     except:
         pass
 
@@ -59,9 +66,15 @@ def solver_local_optimisation(sous_ensemble_objets):
     total_seconde = 0
     liste_ordre = []
     total_mdp = set()
-    #meilleur_element = get_best_element(sous_ensemble_objets, total_mdp)
+
     while total_seconde < limite_seconde_algo:
         meilleur_element = get_best_element(sous_ensemble_objets, total_mdp)
+        print("meilleur element", meilleur_element)
+
+
+        if len(total_mdp | sous_ensemble_objets[meilleur_element]["ensemble"]) == len(total_mdp):
+            break
+
         liste_ordre.append(meilleur_element)
 
         fig.add_trace(go.Scatter(
@@ -70,9 +83,6 @@ def solver_local_optimisation(sous_ensemble_objets):
             mode='markers+lines',
             name=meilleur_element)
         )
-
-        if len(total_mdp | sous_ensemble_objets[meilleur_element]["ensemble"]) == len(total_mdp):
-            break
 
         total_mdp = total_mdp | sous_ensemble_objets[meilleur_element]["ensemble"]
 
@@ -83,18 +93,19 @@ def solver_local_optimisation(sous_ensemble_objets):
         
     fig.update_layout(
         title={
-            'text': "Mots de passe cassés au cours du temps sur les hashs d'entrainement",
+            'text': 'Optimal password cracking sequence',
             'y':0.9,
             'x':0.5,
             'xanchor': 'center',
             'yanchor': 'top'
         },
-        xaxis_title="temps (s)",
-        yaxis_title="Mots de passe cassés",
-        
+        xaxis_title="Time (s)",
+        yaxis_title="Number of recovered passwords",
         )
 
     fig.show()
+
+    fig.write_image("screenshot.png")
 
     return liste_ordre
 
